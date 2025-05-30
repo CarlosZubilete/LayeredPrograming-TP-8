df28ebd5319a47d121edab925d8efcf7d75c377c
CZubileteSanchez <carloszubilete@gmail.com>
2025-05-30 16:47:40 -0300

    Created a new class 'Sucursal' inside the Entities folder

---
+++ Entities/Sucursal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+  public class Sucursal
+  {
+    private int _id;
+    private String _name;
+    private String _address;
+    private String _province; // in Database is a int , CHEKOUT! 
+    private String _description;
+    public Sucursal() { }
+    public int Id
+    {
+      get { return _id; }
+      set { _id = value; }
+    }
+    public String Name
+    {
+      get { return _name; }
+      set { _name = value; }
+    }
+    public String Address
+    {
+      get { return _address; }
+      set { _address = value; }
+    }
+    public String Province
+    {
+      get { return _province; }
+      set { _province = value; }
+    }
+    public String Description
+    {
+      get { return _description; }
+      set { _description = value; }
+    }
+  }
+}
